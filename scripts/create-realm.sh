0e2f593a06cc3dcb53bd226c9536edce5c788dde
marcoantonioni <antonioni.marco@gmail.com>
2024-11-29 14:37:52 +0100

    update

--- scripts/create-realm.sh
+++ scripts/create-realm.sh
@@ -30,36 +30,38 @@ function _getToken() {
 #-------------------------------
 function _createRealm() {
   _newRealmName=$1
-  curl -k -s -X POST "${KEYCLOAK_HOST}/admin/realms" \
-    -H "Authorization: Bearer ${KC_TOKEN}" -H "Accept: application/json" -H "Content-Type: application/json" \
-    -d '{
-      "realm": "'${_newRealmName}'",
-      "displayName": "'${_newRealmName}'",
-      "enabled": true,
-      "sslRequired": "external",
-      "registrationAllowed": false,
-      "loginWithEmailAllowed": true,
-      "duplicateEmailsAllowed": false,
-      "resetPasswordAllowed": false,
-      "editUsernameAllowed": false,
-      "bruteForceProtected": true
-    }'
-
+  if [[ ! -z "${KC_TOKEN}" ]] && [[ ! -z "${_newRealmName}" ]]; then
+    echo "Creating realm [${_newRealmName}]"
+    curl -k -s -X POST "${KEYCLOAK_HOST}/admin/realms" \
+      -H "Authorization: Bearer ${KC_TOKEN}" -H "Accept: application/json" -H "Content-Type: application/json" \
+      -d '{
+        "realm": "'${_newRealmName}'",
+        "displayName": "'${_newRealmName}'",
+        "enabled": true,
+        "sslRequired": "external",
+        "registrationAllowed": false,
+        "loginWithEmailAllowed": true,
+        "duplicateEmailsAllowed": false,
+        "resetPasswordAllowed": false,
+        "editUsernameAllowed": false,
+        "bruteForceProtected": true
+      }'
+  fi
 }
 
 #-------------------------------
 function _createGroup() {
-  #echo "#--- Creating group"
   _realmName=$1
   _grpName=$2
-  echo "Creating group: ${_grpName} in realm: ${_realmName}"
-  curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
-    -X POST "${KEYCLOAK_HOST}/admin/realms/${_realmName}/groups" \
-    -d '{ "name": "'${_grpName}'" }' | jq .
+  if [[ ! -z "${KC_TOKEN}" ]]&& [[ ! -z "${_newRealmName}" ]] && [[ ! -z "${_grpName}" ]]; then
+    echo "Creating group [${_grpName}] in realm [${_realmName}]"
+    curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
+      -X POST "${KEYCLOAK_HOST}/admin/realms/${_realmName}/groups" \
+      -d '{ "name": "'${_grpName}'" }' | jq .
+  fi
 }
 #-------------------------------
 function _createGroups() {
-  echo "#--- Creating groups"
   _realmName=$1
   if [[ ! -z "${KC_TOKEN}" ]] && [[ ! -z "${_realmName}" ]]; then
     for _grpName in "${_GROUPS[@]}"
@@ -74,15 +76,17 @@ function _createUser() {
   #echo "#--- Creating user"
   _realmName=$1
   _usrName=$2
-  echo "Creating user: ${_usrName} in realm: ${_realmName}"
-  curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
-    -X POST "${KEYCLOAK_HOST}/admin/realms/${_realmName}/users" \
-    -d '{ "username": "'${_usrName}'", "firstName":"'${_usrName}'","lastName":"'${_usrName}'", "email":"'${_usrName}'@home.net", "enabled":"true" }'
+  if [[ ! -z "${KC_TOKEN}" ]]&& [[ ! -z "${_realmName}" ]] && [[ ! -z "${_usrName}" ]]; then
+    echo "Creating user [${_usrName}] in realm [${_realmName}]"
+    curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
+      -X POST "${KEYCLOAK_HOST}/admin/realms/${_realmName}/users" \
+      -d '{ "username": "'${_usrName}'", "firstName":"'${_usrName}'","lastName":"'${_usrName}'", "email":"'${_usrName}'@home.net", "enabled":"true" }'
+  fi
 }
 
 #-------------------------------
 function _createUsers() {
-  echo "#--- Creating users"
+  #echo "#--- Creating users"
   _realmName=$1
   if [[ ! -z "${KC_TOKEN}" ]] && [[ ! -z "${_realmName}" ]]; then
     for (( _usrIdx=1; _usrIdx<=$_USER_MAX; _usrIdx++ ))
@@ -117,8 +121,33 @@ _createRoleUsersData() {
 }
 
 #-------------------------------
-function _mapUsersToRoles() {
-  echo "#--- Mapping users to role"
+function _mapUserToGroup() {
+  # echo "#--- Mapping users to role"
+  _realmName=$1
+  _userName=$2
+  _grpName=$3
+  if [[ ! -z "${KC_TOKEN}" ]]&& [[ ! -z "${_realmName}" ]] && [[ ! -z "${_grpName}" ]] && [[ ! -z "${_usrName}" ]]; then
+    echo "Associating user [${_usrName}] to group [${_grpName}] in realm [${_realmName}]"
+    KC_USER_ID=$(curl -s -k -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
+      -X GET "${KEYCLOAK_HOST}/admin/realms/${_realmName}/users" \
+      | jq '.[] | select(.username == "'${_userName}'")' | jq .id | sed 's/"//g')
+
+    KC_GROUP_ID=$(curl -s -k -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
+      -X GET "${KEYCLOAK_HOST}/admin/realms/${_realmName}/groups" \
+      | jq '.[] | select(.name == "'${_grpName}'")' | jq .id | sed 's/"//g')
+
+    if [[ ! -z "${KC_GROUP_ID}" ]] && [[ ! -z "${KC_GROUP_ID}" ]]; then 
+      curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
+        -X PUT "${KEYCLOAK_HOST}/admin/realms/${_realmName}/users/${KC_USER_ID}/groups/${KC_GROUP_ID}"
+    else
+      echo "ERROR IDs not found for user [${_userName}] and/or group [$_grpName]"
+    fi
+  fi
+}
+
+#-------------------------------
+function _mapUsersToGroups() {
+  # echo "#--- Mapping users to role"
   _newRealmName=$1
   _groupIdx=0
   for _grpName in "${_GROUPS[@]}"; do
@@ -126,22 +155,7 @@ function _mapUsersToRoles() {
 
     for _userName in "${_usersInRole[@]}"; do
       echo "Adding user [${_userName}] to group [$_grpName]"
-#---
-      KC_USER_ID=$(curl -s -k -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
-        -X GET "${KEYCLOAK_HOST}/admin/realms/${_newRealmName}/users" \
-        | jq '.[] | select(.username == "'${_userName}'")' | jq .id | sed 's/"//g')
-
-      KC_GROUP_ID=$(curl -s -k -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
-        -X GET "${KEYCLOAK_HOST}/admin/realms/${_newRealmName}/groups" \
-        | jq '.[] | select(.name == "'${_grpName}'")' | jq .id | sed 's/"//g')
-
-      if [[ ! -z "${KC_GROUP_ID}" ]] && [[ ! -z "${KC_GROUP_ID}" ]]; then 
-        curl -s -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer ${KC_TOKEN}" \
-          -X PUT "${KEYCLOAK_HOST}/admin/realms/${_newRealmName}/users/${KC_USER_ID}/groups/${KC_GROUP_ID}"
-      else
-        echo "ERROR IDs not found for user [${_userName}] and/or group [$_grpName]"
-      fi
-
+      _mapUserToGroup "${_newRealmName}" "${_userName}" "${_grpName}"
     done
 
     _groupIdx=$((_groupIdx+1))
@@ -151,17 +165,22 @@ function _mapUsersToRoles() {
 
 _loadGroupAnfUsersFromFile() {
   _realmName="$1"
-  _FILE_NAME="$2"
+  _fileName="$2"
 
-  while IFS="," read -r _GROUP_NAME _USERS
+  while IFS="," read -r _grpName _USERS _userPrefix
   do
-    if [[ "${_GROUP_NAME}" != "GROUP_NAME" ]]; then
-      # echo "Group name: ${_GROUP_NAME}"
+    _grpName="$(echo -e "${_grpName}" | tr -d '[:space:]')"
+    if [[ -z ${_userPrefix} ]]; then
+      _userPrefix="${_USER_PREFIX}"
+    else
+      _userPrefix="$(echo -e "${_userPrefix}" | tr -d '[:space:]')"
+    fi
+    # if not titles
+    if [[ "${_grpName}" != "GROUP_NAME" ]]; then
+      # create goup
+      _createGroup "${_realmName}" "${_grpName}"
 
-      # crea gruppo
-      _createGroup "${_realmName}" "${_GROUP_NAME}"
-
-      # echo "Users: ${_USERS}"
+      # user range ?
       if [[ ${_USERS} = *..* ]]; then
         _trimmed="$(echo -e "${_USERS}" | tr -d '[:space:]')"
         _min=${_trimmed%..*}
@@ -173,21 +192,20 @@ _loadGroupAnfUsersFromFile() {
           if (( $_userSuffix != $_max )); then
             _sep=" "
           fi
-          _USERS+="${_USER_PREFIX}$_userSuffix$_sep"
+          _USERS+="${_userPrefix}$_userSuffix$_sep"
         done
       fi
 
-      #_listOfUsers=()
       for _usr in ${_USERS}; do 
-        #_listOfUsers+=($_usr)
-        #echo "_user: ${_usr}"
+        # create user
         _createUser "${_realmName}" "${_usr}"
 
-        # associa utente a gruppo
+        # associate user to group
+        _mapUserToGroup "${_realmName}" "${_usr}" "${_grpName}"
       done
 
     fi
-  done < <(cat ${_FILE_NAME})
+  done < <(cat ${_fileName})
 }
 
 
@@ -200,8 +218,7 @@ _test() {
   _createRoleUsersData 1 "4..6"
   _createRoleUsersData 2 "7..10"
 
-  _mapUsersToRoles "${_NEW_REALM}"
-
+  _mapUsersToGroups "${_NEW_REALM}"
 }
 
 #-------------------------------
@@ -218,6 +235,8 @@ if [[ ! -z "${_NEW_REALM}" ]]; then
       _loadGroupAnfUsersFromFile "${_NEW_REALM}" "$(pwd)/${_GROUP_USERS_FILE}"
     fi
   fi
+else
+  echo "Usage: $0 'newRealmName' 'pathToGroupsUsersFile'"  
 fi
 
 
